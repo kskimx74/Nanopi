4118bb43122c6fdc5a3dacc2ae408929de2408fa
huyunlei2020 <68583179+huyunlei2020@users.noreply.github.com>
2021-04-05 10:48:02 +0800

    Create cpulimit.lua

---
+++ PATCH/duplicate/luci-app-cpulimit/luasrc/controller/cpulimit.lua
@@ -0,0 +1,12 @@
+module("luci.controller.cpulimit", package.seeall)
+
+function index()
+	if not nixio.fs.access("/etc/config/cpulimit") then
+		return
+	end
+	
+	local page = entry({"admin", "system", "cpulimit"}, cbi("cpulimit"), luci.i18n.translate("cpulimit"), 65)
+	page.i18n = "cpulimit"
+	page.dependent = true
+	
+end
